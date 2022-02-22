a082e0c6d624b42b6f5edf5f817ee4d5663a28b4
Robin Song <robin.song@edmonton.ca>
2022-02-21 20:47:28 -0700

    update

--- funcs.py
+++ funcs.py
@@ -1,6 +1,16 @@
 import vtk
 import slicer
 
+def show_3Dviews():
+    layoutManager = slicer.app.layoutManager()
+    for threeDViewIndex in range(layoutManager.threeDViewCount) :
+        view = layoutManager.threeDWidget(threeDViewIndex).threeDView()
+        threeDViewNode = view.mrmlViewNode()
+        cameraNode = slicer.modules.cameras.logic().GetViewActiveCameraNode(threeDViewNode)
+        print("View node for 3D widget " + str(threeDViewIndex))
+        print("  Name: " + threeDViewNode .GetName())
+        print("  ID: " + threeDViewNode .GetID())
+        print("  Camera ID: " + cameraNode.GetID())
 
 def showVolumeRendering(volumeNode, adjust_preset=False):
     print("Show volume rendering of node " + volumeNode.GetName())
@@ -10,9 +20,9 @@ def showVolumeRendering(volumeNode, adjust_preset=False):
     displayNode.SetVisibility(True)
 
     # croping
-    roiNode = displayNode.GetMarkupsROINode()
-    displayNode.SetCroppingEnabled(True)
-    roiNode.GetDisplayNode().SetVisibility(True)
+    # roiNode = displayNode.GetMarkupsROINode()
+    # displayNode.SetCroppingEnabled(True)
+    # roiNode.GetDisplayNode().SetVisibility(True)
 
     scalarRange = volumeNode.GetImageData().GetScalarRange()
     if adjust_preset:
@@ -26,27 +36,29 @@ def showVolumeRendering(volumeNode, adjust_preset=False):
                 volRenLogic.GetPresetByName("CT-Chest-Contrast-Enhanced"))
 
 
-def ShowVolumePlaneCut(volumeNode, iren):
+def ShowVolumePlaneCut(volumeData, renWin):
     # Define volume mapper
     volumeMapper = vtk.vtkSmartVolumeMapper()
     # volumeMapper = vtk.vtkVolumeMapper()
-    volumeMapper.SetInputData(volumeNode)
+    volumeMapper.SetInputData(volumeData)
     # add plane cutter
     planeWidget = vtk.vtkImplicitPlaneWidget()
     # vtk.vtkImplicitPlaneWidget2()
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
     planeWidget.SetInteractor(iren)
     planeWidget.SetPlaceFactor(1.0)
     # renderer.AddVolume(volume)
     # interactor.Render()
     plane = vtk.vtkPlane()
-    center = volumeNode.GetCenter()
-    plane.SetOrigin(center)
+    # center = volumeData.GetCenter()
+    # plane.SetOrigin(center)
 
     def clipVolumeRender(obj, event):
         obj.GetPlane(plane)
         volumeMapper.AddClippingPlane(plane)
 
-    planeWidget.SetInputData(volumeNode.GetMapper().GetInput())
+    # planeWidget.SetInputData(volumeData)
     planeWidget.GetSelectedOutlineProperty().SetColor(1, 0, 1)
     planeWidget.GetOutlineProperty().SetColor(0.2, 0.2, 0.2)
     planeWidget.GetOutlineProperty().SetOpacity(0.7)
@@ -58,13 +70,3 @@ def ShowVolumePlaneCut(volumeNode, iren):
     planeWidget.On()
     planeWidget.AddObserver("InteractionEvent", clipVolumeRender)
 
-def show_3Dviews():
-    layoutManager = slicer.app.layoutManager()
-    for threeDViewIndex in range(layoutManager.threeDViewCount) :
-        view = layoutManager.threeDWidget(threeDViewIndex).threeDView()
-        threeDViewNode = view.mrmlViewNode()
-        cameraNode = slicer.modules.cameras.logic().GetViewActiveCameraNode(threeDViewNode)
-        print("View node for 3D widget " + str(threeDViewIndex))
-        print("  Name: " + threeDViewNode .GetName())
-        print("  ID: " + threeDViewNode .GetID())
-        print("  Camera ID: " + cameraNode.GetID())
